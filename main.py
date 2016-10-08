3463d4a46ee19fbc3bc5a26678206720fd7c7ca2
Adrian L Lange <mail@p3lim.net>
2016-10-08 09:35:49 +0200

    Bare bones

---
+++ main.py
@@ -0,0 +1,16 @@
+import ScreenCloud
+
+class CaddyUpload():
+	def __init__(self):
+		pass
+
+	def upload(self, screenshot, name):
+		return True
+
+	def getFilename(self):
+		return ScreenCloud.formatFilename('temp')
+
+	def showSettingsUI(self, parentWidget):
+
+	def isConfigured(self):
+		return True
