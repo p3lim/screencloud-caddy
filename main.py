776d246cfdf5769d69395c1c6916d7fa35eca130
Adrian L Lange <mail@p3lim.net>
2016-10-08 10:12:48 +0200

    Add authentication support

--- main.py
+++ main.py
@@ -24,7 +24,10 @@ class CaddyUpload():
 			return False
 
 		try:
-			url = caddy.upload(self.url_address, path)
+			if self.auth_enable and len(self.auth_key) > 0 and len(self.auth_secret) > 0:
+				url = caddy.upload(self.url_address, path, self.auth_key, self.auth_secret)
+			else:
+				url = caddy.upload(self.url_address, path)
 		except Exception as e:
 			ScreenCloud.setError('\n' + str(e))
 			return False
@@ -41,13 +44,18 @@ class CaddyUpload():
 		self.parentWidget = parentWidget
 
 		self.settingsDialog = self.uil.load(QFile(workingDir + '/settings.ui'), parentWidget)
+		self.settingsDialog.group_url.check_auth.connect('stateChanged(int)', self.updateUI)
 		self.settingsDialog.group_screenshot.input_name.connect('textChanged(QString)', self.nameFormatEdited)
 		self.settingsDialog.connect('accepted()', self.saveSettings)
 
 		self.loadSettings()
 
 		self.settingsDialog.group_url.input_url.text = self.url_address
+		self.settingsDialog.group_url.check_auth.checked = self.auth_enable
+		self.settingsDialog.group_url.input_key.text = self.auth_key
+		self.settingsDialog.group_url.input_secret.text = self.auth_secret
 		self.settingsDialog.group_url.check_copylink.checked = self.copy_link
+
 		self.settingsDialog.group_screenshot.input_name.text = self.name_format
 
 		self.updateUI()
@@ -60,7 +68,11 @@ class CaddyUpload():
 		settings.beginGroup('caddy')
 
 		self.url_address = settings.value('url-address', '')
+		self.auth_enable = settings.value('auth-enable', 'False') == 'True'
+		self.auth_key = settings.value('auth-key', '')
+		self.auth_secret = settings.value('auth-secret', '')
 		self.copy_link = settings.value('copy-link', 'True') == 'True'
+
 		self.name_format = settings.value('name-format', '%Y-%m-%d_%H-%M-%S')
 
 		settings.endGroup()
@@ -72,7 +84,11 @@ class CaddyUpload():
 		settings.beginGroup('caddy')
 
 		settings.setValue('url-address', self.settingsDialog.group_url.input_url.text)
+		settings.setValue('auth-enable', str(self.settingsDialog.group_url.check_auth.checked))
+		settings.setValue('auth-key', self.settingsDialog.group_url.input_key.text)
+		settings.setValue('auth-secret', self.settingsDialog.group_url.input_secret.text)
 		settings.setValue('copy-link', str(self.settingsDialog.group_url.check_copylink.checked))
+
 		settings.setValue('name-format', self.settingsDialog.group_screenshot.input_name.text)
 
 		settings.endGroup()
@@ -82,7 +98,20 @@ class CaddyUpload():
 		self.loadSettings()
 
 		if len(self.url_address) > 0:
-			return True
+			if self.auth_enable:
+				if len(self.auth_key) > 0 and len(self.auth_secret) > 0:
+					return True
+			else:
+				return True
 
 	def nameFormatEdited(self, name_format):
 		self.settingsDialog.group_screenshot.label_example.setText(ScreenCloud.formatFilename(name_format))
+
+	def updateUI(self):
+		auth_enabled = self.settingsDialog.group_url.check_auth.checked
+
+		self.settingsDialog.group_url.label_key.setVisible(auth_enabled)
+		self.settingsDialog.group_url.input_key.setVisible(auth_enabled)
+
+		self.settingsDialog.group_url.label_secret.setVisible(auth_enabled)
+		self.settingsDialog.group_url.input_secret.setVisible(auth_enabled)
